52dc62ae9c3fdf7e7c5f80c6cda95bcd70ec6c26
Sudipa Acharya <sudipaacharya351@gmail.com>
2023-01-19 02:30:16 +0530

    code added

---
+++ app.js
@@ -0,0 +1,12 @@
+const thembutton = document.querySelector('.nav-theme');
+const themeList = document.querySelector('.theme-list');
+
+thembutton.addEventListener('mouseover', show);
+
+function show(event) {
+  event.preventDefault();
+themeList.classList.add('hidden');
+  console.log('hello');
+}
+//console.log(thembutton);
+//console.log(themeList);
